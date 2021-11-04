7e4f75ccb0465140ec3851d296d9646e1396eb3b
Kade Allen <kadeallenprofessional@gmail.com>
2021-11-04 14:45:12 +1300

    Fixed health check
    Fixed tests and got it working

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,22 @@
+use std::net::TcpListener;
+use zero2prod::run;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -13,6 +25,3 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    todo!()
-}
